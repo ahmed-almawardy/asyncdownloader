78458252dad6e5dfdccd04d541c0c48cdb19ff2f
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2024-07-14 20:27:56 +0300

    Fix code style

--- src/services.py
+++ src/services.py
@@ -1,45 +1,49 @@
-"""
- Services to provide additional helpers to the main app
-"""
+"""Services to provide additional helpers to the main app."""
+
 import base64
 import hashlib
-import logging
 from datetime import datetime
-from logging import getLogger
+from logging import INFO, FileHandler, StreamHandler, getLogger
+
 from settings import DATE_FORMATE
 
-logger = getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.FileHandler('../app.log', mode='a+', ))
+logger = getLogger('APP')
+logger.setLevel(INFO)
+logger.addHandler(StreamHandler())
+logger.addHandler(FileHandler('../app.log', mode='a+'))
 
 
 def log_started(started_at):
     """
-    Logger head
+    Write Logger head.
+
     :param started_at:  datetime.datetime
     :return:
     """
-    logger.info(f"{logger.name}: Starting app... at {started_at:{DATE_FORMATE}}")
+    logger.info(f'{logger.name}: Starting app... {started_at:{DATE_FORMATE}}')
 
 
 def log_ended(started_at):
     """
-    Logger tail
+    Write Logger tail.
+
     :param started_at:  datetime.datetime
     :return:
     """
-    logger.info(f"{logger.name}: Done. at {datetime.now():{DATE_FORMATE}}")
+    formated_date = datetime.now().strftime(DATE_FORMATE)
+    logger.info(f'{logger.name}: Done. at {formated_date}')
     timing = datetime.now() - started_at
-    logger.info(f"{logger.name}: timing. {timing.total_seconds()} secs/mcs")
-    logger.info('=' * 120)
+    logger.info(f'{logger.name}: timing. {timing.total_seconds()} secs/ms')
+    line_length = 120
+    logger.info('=' * line_length)
 
 
-def hash_content(content: bytes) -> str:
+def hash_content(file_content: bytes) -> str:
     """
-    used for generating sha256 hash for raw buffered data.
-    :param content: bytes data
+    Use for generating sha256 hash for raw buffered data.
+
+    :param file_content: bytes data
     :return: str sha256 hash
     """
-    encoded_b64 = base64.b64encode(content)
+    encoded_b64 = base64.b64encode(file_content)
     return hashlib.sha256(encoded_b64).hexdigest()
