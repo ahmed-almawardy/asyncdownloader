64d6374593b499149bf9c9c44ce60d42356e4ac7
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2024-07-15 00:19:24 +0300

    prepare tests

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,9 +1,3 @@
 from unittest import mock
 
 
-@mock.patch('app.App.run', return_value=None)
-def test_app_runs(stub_run):
-    assert stub_run() is None
-
-
-
--- tests/unit/test_httpclient.py
+++ tests/unit/test_httpclient.py
@@ -11,22 +11,7 @@ pytestmark = [
 ]
 
 
-async def test_client_get_url():
-    dependency_client = mock.AsyncMock(spec=AsyncClient)
-    with patch.object(AsyncHttpClient, 'get') as mocked_get:
-        http_instance = AsyncHttpClient(dependency_client)
 
-        await http_instance.get("http://example.com")
-        mocked_get.assert_called_once_with("http://example.com")
-
-
-async def test_client_download_url_to_directory():
-    dependency_client = mock.AsyncMock(spec=AsyncClient)
-    with patch.object(AsyncHttpClient, 'download') as mocked_get:
-        http_instance = AsyncHttpClient(dependency_client)
-
-        await http_instance.download("http://example.com", "./download_here")
-        mocked_get.assert_called_once_with("http://example.com", "./download_here")
 
 
 
