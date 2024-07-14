a8f3ba4d3fc1cc961b2394a48d04c388b52d8647
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2024-07-15 00:40:15 +0300

    prepare tests

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,3 +1,8 @@
 from unittest import mock
+from src.app import App, main
 
 
+@mock.patch.object(App, 'download_repo_structure')
+async def test_main_internals(mocked_download_repo_structure):
+    await main()
+    mocked_download_repo_structure.assert_called_once()
